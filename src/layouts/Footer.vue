b70fc89f7b70812c05d698f5d264653f7dfd4375
huahhuah <huah734@gmail.com>
2025-05-03 17:31:43 +0800

    feat: 新增登入與註冊頁面

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,11 +1,51 @@
 <template>
-  <footer class="bg-dark text-white mt-auto py-3">
-    <div class="container text-center">
-      <p class="mb-0">© 2025 Loveia 募資平台 | 偏鄉醫療與愛心同行</p>
+  <footer class="footer py-4 mt-5">
+    <div class="container">
+      <div class="row">
+        <!-- 左側：Logo 與社群 -->
+        <div class="col-md-3 mb-3 text-center text-md-start">
+          <img src="/homepageS1-logo.png" alt="Lovia Logo" width="100" />
+          <div class="mt-3 d-flex justify-content-center justify-content-md-start gap-3">
+            <a href="#"><i class="bi bi-instagram fs-4 text-dark"></i></a>
+            <a href="#"><i class="bi bi-facebook fs-4 text-dark"></i></a>
+            <a href="#"><i class="bi bi-envelope-fill fs-4 text-dark"></i></a>
+          </div>
+        </div>
+
+        <!-- 中間：連結 -->
+        <div class="col-md-3 mb-3 text-center text-md-start">
+          <ul class="list-unstyled small">
+            <li><a href="#" class="text-dark text-decoration-none">幫助</a></li>
+            <li><a href="#" class="text-dark text-decoration-none">常見問題</a></li>
+            <li><a href="#" class="text-dark text-decoration-none">隱私權政策</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3 mb-3 text-center text-md-start">
+          <ul class="list-unstyled small">
+            <li><a href="#" class="text-dark text-decoration-none">關於我們</a></li>
+            <li><a href="#" class="text-dark text-decoration-none">聯絡我們</a></li>
+            <li><a href="#" class="text-dark text-decoration-none">授權說明</a></li>
+          </ul>
+        </div>
+
+        <!-- 右側：說明文字 -->
+        <div class="col-md-3 small text-center text-md-start">
+          <strong>公益串聯無縫改變世界</strong>
+          <p class="mt-2">
+            我們的平台旨在為善舉組織與公益行動者打造的橋梁，讓更多有愛心的人能夠互相支持與幫助。邀請您一同關注社會的每個角落，攜手推動公益，讓世界更美好！
+          </p>
+        </div>
+      </div>
+
+      <div class="text-center mt-4 small text-muted">
+        © Copyright 2025 北十有限公司
+      </div>
     </div>
   </footer>
 </template>
 
-<script setup>
-// 無邏輯可省略 script
-</script>
+<style scoped>
+.footer {
+  background-color: #ffd5d5;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,13 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/test",
-      name: "test",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/TestView.vue"),
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
+    },{
+      path: "/register",
+      name: "register",
+      component: () => import("../views/RegisterForm.vue"),
     },
   ],
 });
