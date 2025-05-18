96bb70a8c9dfee258769aca283b09b5d1cfba527
PJA0103 <anne0103@outlook.com>
2025-05-18 15:39:11 +0800

    get_progress_API#38

--- src/views/ProjectDetailView.vue
+++ src/views/ProjectDetailView.vue
@@ -36,6 +36,28 @@
         <p><strong>出貨日期:</strong>{{ plan.delivery_date }}</p>
       </div>
     </section>
+
+    <!--顯示專案進度-->
+    <section v-if="progresses.length">
+      <h2>專案進度</h2>
+      <div v-for="(progress, index) in progresses"
+           :key="index"
+           style="border: 1px solid #ccc; padding: 1rem; margin-bottom: 1rem"
+      >
+        <h3>{{ progress.title }} ({{progress.date}})</h3>
+        <p>{{ progress.content }}</p>
+        <h4>資金使用</h4>
+        <ul>
+          <li v-for="(usage, index) in progress.fund_usages" :key="index">
+            <strong>受款方:</strong> {{ usage.recipient }} <br />
+            <strong>用途:</strong> {{ usage.usage }} <br />
+            <strong>金額:</strong> {{ usage.amount.toLocaleString() }} 元 <br />
+            <strong>狀態:</strong> {{ usage.status || '無狀態' }}
+          </li>
+        </ul>
+      </div>
+    </section>
+
   </div>
   <div v-else>
     <p>載入中...</p>
@@ -45,11 +67,12 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
-import { getProjectById } from '../api/project.js'
+import { getProjectById, getProgress } from '../api/project.js'
 import axios from 'axios'
 
 const route = useRoute() // 取得當前路由
 const project = ref(null) // 定義 project 資料
+const progresses = ref([]) 
 
 onMounted(async () => {
   const id = route.params.id // 從路由參數取得專案 id
@@ -59,7 +82,15 @@ onMounted(async () => {
       project.value = res.data.data // 存儲資料
       console.log('專案資料', project.value)
     } else {
-      console.error('API錯誤', err)
+      console.error('API錯誤', res)
+    }
+
+    // 取得專案進度
+    const progressRes = await getProgress(id) 
+    if (progressRes.status === 200 && progressRes.data.status === true) {
+      progresses.value = progressRes.data.data // 存儲進度資料
+    } else {
+      console.error('進度資料錯誤', progressRes)
     }
   } catch (err) {
     console.error('取得專案失敗', err)
