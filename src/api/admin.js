e72487fbeaf60ffd4f0e3c6689d8ec1d0c966a85
PJA0103 <anne0103@outlook.com>
2025-06-16 00:11:47 +0800

    revise auth fulfill admin

--- src/api/admin.js
+++ src/api/admin.js
@@ -5,14 +5,12 @@ const BASE_URL = 'https://lovia-backend-xl4e.onrender.com'
 
 // 取得全部使用者資料
 export const allUsers = ( token, page = 1 ) => {
-  return axios.get(`${BASE_URL}/api/v1/admins/users`{
-    params:{ page, }
-  // return axios.get(`${BASE_URL}/api/v1/admins/users`, {
-  //   headers: {
-  //     Authorization: `Bearer ${token}`,
-  //   },
-    // params:{
-    //   page,
-    // }
+  return axios.get(`${BASE_URL}/api/v1/admins/users`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params:{
+      page,
+    }
   })
 }
